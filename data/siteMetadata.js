765394f03899a2a6cde161208aa7702eefb50b31
Kazuya Yokoyama <yk3kzy@gmail.com>
2023-12-30 09:00:23 +0900

    fix metadata

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -3,10 +3,10 @@ const siteMetadata = {
   title: 'to be continued',
   author: 'ykk',
   headerTitle: 'to be continued',
-  description: 'A blog created with Next.js and Tailwind.css',
+  description: 'ykk による個人開発ブログ、ときどき趣味',
   language: 'ja-jp',
   theme: 'light', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
+  siteUrl: 'https://to-be-continued.vercel.app',
   siteRepo: 'https://github.com/Kaaaaazuya/ykk-blog',
   siteLogo: '/static/images/logo.svg',
   socialBanner: '/static/images/twitter-card.png',
@@ -18,17 +18,17 @@ const siteMetadata = {
     // If you want to use an analytics provider you have to add it to the
     // content security policy in the `next.config.js` file.
     // supports Plausible, Simple Analytics, Umami, Posthog or Google Analytics.
-    umamiAnalytics: {
-      // We use an env variable for this site to avoid other users cloning our analytics ID
-      umamiWebsiteId: process.env.NEXT_UMAMI_ID, // e.g. 123e4567-e89b-12d3-a456-426614174000
-    },
+    // umamiAnalytics: {
+    //   // We use an env variable for this site to avoid other users cloning our analytics ID
+    //   umamiWebsiteId: process.env.NEXT_UMAMI_ID, // e.g. 123e4567-e89b-12d3-a456-426614174000
+    // },
     // plausibleAnalytics: {
     //   plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
     // },
     // simpleAnalytics: {},
-    // posthogAnalytics: {
-    //   posthogProjectApiKey: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
-    // },
+    posthogAnalytics: {
+      posthogProjectApiKey: process.env.NEXT_POSTHOG_ID,
+    },
     // googleAnalytics: {
     //   googleAnalyticsId: '', // e.g. G-XXXXXXX
     // },
